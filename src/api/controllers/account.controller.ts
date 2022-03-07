d60c761a8199205f5a6edb81993b8a7f4c72aec2
Paulo Garnica <paulo.garnica@gmail.com>
2022-03-07 16:58:09 -0300

    add catch for errors

--- src/api/controllers/account.controller.ts
+++ src/api/controllers/account.controller.ts
@@ -2,43 +2,54 @@ import { Request, Response } from "express";
 import { IAccountDto, Account } from "../models/account";
 
 class AccountController {
-  
   //getById Method
   async getById(req: Request, res: Response) {
-    const account = await Account.findById(req.params.id);
-    if (account) {
-      const accountDto: IAccountDto = {
-        name: account.name,
-        balance: account.balance
-      };
-      res.status(200).json({ account: accountDto });
-    }else
-    {
-      res.status(404).send("Account not found.");
+    try {
+      const account = await Account.findById(req.params.id);
+      if (account) {
+        const accountDto: IAccountDto = {
+          name: account.name,
+          balance: account.balance,
+        };
+        res.status(200).json({ account: accountDto });
+      } else {
+        res.status(404).send("Account not found.");
+      }
+    } catch (error) {
+      const { code, message }: any = error;
+      return res.status(400).json({ code, message });
     }
   }
 
   //get Method
   async get(req: Request, res: Response) {
-    const accounts = await Account.find();
-    if (accounts) {
-      res.status(200).json({ accounts });
-    } else {
-      res.status(404).send("There is no account data.");
+    try {
+      const accounts = await Account.find();
+      if (accounts) {
+        res.status(200).json({ accounts });
+      } else {
+        res.status(404).send("There is no account data.");
+      }
+    } catch (error) {
+      const { code, message }: any = error;
+      return res.status(400).json({ code, message });
     }
   }
 
-    //getBalance Method
-    async getBalance(req: Request, res: Response) {
-        const account = await Account.findById(req.params.id);
-        if (account) {
-          res.status(200).json({ balance: account.balance });
-        }else
-        {
-          res.status(404).send("Account not found.");
-        }
+  //getBalance Method
+  async getBalance(req: Request, res: Response) {
+    try {
+      const account = await Account.findById(req.params.id);
+      if (account) {
+        res.status(200).json({ balance: account.balance });
+      } else {
+        res.status(404).send("Account not found.");
       }
-
+    } catch (error) {
+      const { code, message }: any = error;
+      return res.status(400).json({ code, message });
+    }
+  }
 }
 
 export default new AccountController();
--- src/api/controllers/person.controller.ts
+++ src/api/controllers/person.controller.ts
@@ -1,21 +1,23 @@
 import { Request, Response } from "express";
 import { IPersonDto, Person } from "../models/person";
 import { IAccountDto, Account } from "../models/account";
-import {encrypt} from '../../crypto'
+import { encrypt } from "../../crypto";
 
-const validateUniqueFields =  async (email:string, cpf:string) =>
-{
+const validateUniqueFields = async (email: string, cpf: string) => {
   if (await Person.findOne({ email })) {
-    return { status: 400,
-             message: 'There is already a person with the given email!'};
-  }else  if (await Person.findOne({ cpf })) {
-    return { status: 400,
-      message: 'There is already a person with the given CPF!'};
-  }else  {
-    return { status: 200,
-      message: 'Ok!'};
+    return {
+      status: 400,
+      message: "There is already a person with the given email!",
+    };
+  } else if (await Person.findOne({ cpf })) {
+    return {
+      status: 400,
+      message: "There is already a person with the given CPF!",
+    };
+  } else {
+    return { status: 200, message: "Ok!" };
   }
-}
+};
 
 class PersonController {
   //POST Method
@@ -25,23 +27,22 @@ class PersonController {
       const { email, cpf, password } = req.body;
 
       //check if already exists person with the given email and cpf
-      var  validation = await validateUniqueFields(email,cpf);
+      var validation = await validateUniqueFields(email, cpf);
 
-      if(validation.status !== 200)
-      {
-        return res.status(validation.status).json({ message:validation.message });
+      if (validation.status !== 200) {
+        return res
+          .status(validation.status)
+          .json({ message: validation.message });
       }
-       
+
       const person = await Person.create(req.body);
-      if(person)
-      {
-        const accountDto: IAccountDto =
-        {
-          name : email,
+      if (person) {
+        const accountDto: IAccountDto = {
+          name: email,
           balance: 0,
           password: encrypt(password),
-          person : person._id
-        }
+          person: person._id,
+        };
         await Account.create(accountDto);
       }
 
@@ -54,74 +55,94 @@ class PersonController {
 
   //getById Method
   async getById(req: Request, res: Response) {
-    const person = await Person.findById(req.params.id);
-    if (person) {
-      const personDto: IPersonDto = {
-        name: person.name,
-        gender: person.gender,
-        birthDate: person.birthDate.toLocaleDateString(),
-        email: person.email,
-        cpf:person.cpf
-      };
-      res.status(200).json({ person: personDto });
-    }else
-    {
-      res.status(404).send("Person not found.");
+    try {
+      const person = await Person.findById(req.params.id);
+      if (person) {
+        const personDto: IPersonDto = {
+          name: person.name,
+          gender: person.gender,
+          birthDate: person.birthDate.toLocaleDateString(),
+          email: person.email,
+          cpf: person.cpf,
+        };
+        res.status(200).json({ person: personDto });
+      } else {
+        res.status(404).send("Person not found.");
+      }
+    } catch (error) {
+      const { code, message }: any = error;
+      return res.status(400).json({ code, message });
     }
   }
 
   //get Method
   async get(req: Request, res: Response) {
-    const persons = await Person.find();
-    if (persons) {
-      res.status(200).json({ persons: persons });
-    } else {
-      res.status(404).send("There is no person data.");
+    try {
+      const persons = await Person.find();
+      if (persons) {
+        res.status(200).json({ persons: persons });
+      } else {
+        res.status(404).send("There is no person data.");
+      }
+    } catch (error) {
+      const { code, message }: any = error;
+      return res.status(400).json({ code, message });
     }
   }
 
   //put Method
   async put(req: Request, res: Response) {
-    const { id } = req.params;
-    if (!id) {
-      res.status(400).send("The Id parameter is mandatory.");
-    }
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).send("The Id parameter is mandatory.");
+      }
 
-    const { email, cpf } = req.body;
+      const { email, cpf } = req.body;
 
-    //check if already exists person with the given email and cpf
-    var  validation = await validateUniqueFields(email,cpf);
+      //check if already exists person with the given email and cpf
+      var validation = await validateUniqueFields(email, cpf);
 
-    if(validation.status !== 200)
-    {
-      return res.status(validation.status).json({ message:validation.message });
-    }
+      if (validation.status !== 200) {
+        return res
+          .status(validation.status)
+          .json({ message: validation.message });
+      }
 
-    const person = await Person.findOneAndUpdate({ _id: id }, req.body, {
-      new: true,
-      runValidators: true,
-    });
+      const person = await Person.findOneAndUpdate({ _id: id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
 
-    if (person) {
-      res.status(200).json({ person });
-    } else {
-      res.status(404).send("Person not found.");
+      if (person) {
+        res.status(200).json({ person });
+      } else {
+        res.status(404).send("Person not found.");
+      }
+    } catch (error) {
+      const { code, message }: any = error;
+      return res.status(400).json({ code, message });
     }
   }
 
   //delete Method
   async delete(req: Request, res: Response) {
-    const { id } = req.params;
-    if (!id) {
-      res.status(400).send("The Id parameter is mandatory.");
-    }
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).send("The Id parameter is mandatory.");
+      }
 
-    const person = await Person.findOneAndDelete({ _id: id });
+      const person = await Person.findOneAndDelete({ _id: id });
 
-    if (person) {
-      res.status(200).send("Person successfully deleted");
-    } else {
-      res.status(404).send("Person not found.");
+      if (person) {
+        res.status(200).send("Person successfully deleted");
+      } else {
+        res.status(404).send("Person not found.");
+      }
+    } catch (error) {
+      const { code, message }: any = error;
+      return res.status(400).json({ code, message });
     }
   }
 }
